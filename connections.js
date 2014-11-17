1b8e7bdfbb078ff37eb11f963e9d1ecd428c9013
eschmuck <Eric.Schmuck@Philips.com>
2014-11-17 10:08:12 -0500

    Create connections.js

---
+++ connections.js
@@ -0,0 +1,11 @@
+exports.CON_PLAYING    = 0;
+exports.CON_GET_NAME   = 2; // By what name ..?
+exports.CON_NAME_CNFRM = 3; // Did I get that right, x?
+exports.CON_PASSWORD   = 4; // Password:
+exports.CON_NEWPASSWD  = 5;	// Give me a password for x
+exports.CON_CNFPASSWD  = 6;	// Please retype password:
+exports.CON_QSEX       = 7; // Sex?
+exports.CON_CON_QCLASS = 8; // Class?
+exports.CON_RMOTD      = 9;
+exports.CON_MENU       = 10;
+
