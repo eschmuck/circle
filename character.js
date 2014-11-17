d780aeb37ebcae2d6a76216949306a35e6cfb8db
eschmuck <Eric.Schmuck@Philips.com>
2014-11-17 10:02:47 -0500

    Create character.js

---
+++ character.js
@@ -0,0 +1,31 @@
+// Object constructor
+function Character() {
+	this.inventory = [];
+	this.fighting = null;
+	this.room = null;
+}
+
+// Public Properties
+Character.prototype.name;
+Character.prototype.gender;
+
+Character.prototype.hitpoints;
+Character.prototype.maximumHitpoints;
+Character.prototype.manapoints;
+Character.prototype.maximumManapoints;
+Character.prototype.movepoints;
+Character.prototype.maximumMovepoints;
+
+Character.prototype.gold;
+Character.prototype.experience;
+Character.prototype.level;
+
+Character.prototype.position;
+
+// Constants
+exports.GENDER_NEUTRAL = 0;
+exports.GENDER_MALE    = 1;
+exports.GENDER_FEMALE  = 2;
+
+// Exports
+module.exports = Character;
