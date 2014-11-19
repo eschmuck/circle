779c41aa04044534c3ee131336ef1d563aeb59b5
eschmuck <e_schmuck@yahoo.com>
2014-11-19 20:05:44 +0000

    modified:   app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 var path = require('path');
 var socketio = require('socket.io');
 
+var character = require('./character');
 var connections = require('./connections');
 var database = require('./database');
 var player = require('./player');
@@ -26,6 +27,7 @@ var welcomeMessage = 'Welcome to CircleMUD 4.0\n\rBy what name do you wish to be
 var menu = 'Welcome to CircleMUD!\n\r0) Exit from CircleMUD.\n\r1) Enter the game.\n\r\n\r  Make a choice: ';
 var startMessage = 'Welcome.  This is your new CircleMUD character!  You can now earn gold, gain experience, find weapons and equipment, and much more -- while meeting people from around the world!';
 var motd = 'This is the message of the day.';
+var classMenu = '\r\nSelect a class:\r\n  [C]leric\r\n  [T]hief\r\n  [W]arrior\r\n  [M]agic-user\r\n';
 
 io.sockets.on('connection', function(socket) {
   console.log('A new user connected!');
@@ -69,6 +71,64 @@ io.sockets.on('connection', function(socket) {
           socket.emit('message', motd + '\n\r*** PRESS RETURN: ');
         }
         break;
+      case connections.CON_NEWPASSWD:
+        if(msg['input'].length < 3 || msg['input'].length > 10) {
+          socket.emit('message', 'Illegal password.\n\rPassword: ');
+        }
+        else {
+          socket.player.password = msg['input'];
+          socket.emit('message', 'Please retype password: ');
+          socket.connectionState = connections.CON_CNFPASSWD;
+        }
+        break;
+      case connections.CON_CNFPASSWD:
+        if(msg['input'] !== socket.player.password) {
+          socket.emit('message', 'Passwords don\'t match... start over.\n\rPassword:');
+          socket.connectionState = connections.CON_PASSWORD;
+        }
+        else {
+          socket.emit('message', 'What is your sex (M/F)?');
+          socket.connectionState = connections.CON_QSEX;
+        }
+        break;
+      case connections.CON_QSEX:
+        var sexInput = msg['input'].substring(0, 1).toUpperCase();
+        
+        if(sexInput === 'M') {
+          socket.player.gender = character.GENDER_MALE;
+          socket.connectionState = connections.CON_QCLASS;
+          socket.emit('message', classMenu);
+        }
+        else if(sexInput === 'F') {
+          socket.player.gender = character.GENDER_MALE;
+          socket.connectionState = connections.CON_QCLASS;
+          socket.emit('message', classMenu);
+        }
+        else {
+          socket.emit('message', 'That is not a sex... What IS your sex (M/F)?');
+        }
+        break;
+      case connections.CON_QCLASS:
+        var classInput = msg['input'].substring(0, 1).toUpperCase();
+        if(classInput === 'C') {
+          socket.player.class = player.CLASS_CLERIC;
+        }
+        else if(classInput === 'M') {
+          socket.player.class = player.CLASS_MAGIC_USER;
+        }
+        else if(classInput === 'W') {
+          socket.player.class = player.CLASS_WARRIOR;
+        }
+        else if(classInput === 'T') {
+          socket.player.class = player.CLASS_THIEF;
+        }
+        else {
+          socket.emit('message', '\r\nThat\'s not a class.\r\nClass: ');
+          return;
+        }
+        socket.emit('message', motd + '\n\r*** PRESS RETURN: ');
+        socket.connectionState = connections.CON_RMOTD;
+        break;
       case connections.CON_RMOTD:
         socket.connectionState = connections.CON_MENU;
         socket.emit('message', menu);
@@ -90,9 +150,9 @@ io.sockets.on('connection', function(socket) {
   });
   
   function afterPlayerLoaded(playerDocument) {
-    console.log('Got here!');
     if(playerDocument === null) {
       socket.emit('message', 'Did I get that right, ' + socket.player.name + ' (Y/N)?' );
+      socket.connectionState = connections.CON_NAME_CNFRM;
     }
   }
 });
