b2d37940c83f0499e33fd03425144f5da46a4664
eschmuck <e_schmuck@yahoo.com>
2014-12-12 18:29:47 +0000

    modified:   zone.js

--- zone.js
+++ zone.js
@@ -1,27 +1,27 @@
-var mongoose = require('mongoose');
-var schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var schema = mongoose.Schema;
 
-var zoneSchema = new schema({
-    id: Number,
-    name: String,
-    lowestRoomNumber: Number,
-    highestRoomNumber: Number,
-    lifespan: Number,
-    resetMode: Number,
-    resetCommands: [],
-    age: Number
-});
+// var zoneSchema = new schema({
+//     id: Number,
+//     name: String,
+//     lowestRoomNumber: Number,
+//     highestRoomNumber: Number,
+//     lifespan: Number,
+//     resetMode: Number,
+//     resetCommands: [],
+//     age: Number
+// });
 
-zoneSchema.methods.reset = function(rooms) {
-    // for(var i = 0; i < this.resetCommands.length; i++) {
-    //     console.log(this.resetCommands[i]);
-    // }
-};
+// zoneSchema.methods.reset = function(rooms) {
+//     // for(var i = 0; i < this.resetCommands.length; i++) {
+//     //     console.log(this.resetCommands[i]);
+//     // }
+// };
 
-var zoneModel = mongoose.model('zone', zoneSchema);
+// var zoneModel = mongoose.model('zone', zoneSchema);
 
-exports.getZones = function getZones(callback) {
-	zoneModel.find({}, function(err, docs) {
-		callback(docs);
-	});
-};
+// exports.getZones = function getZones(callback) {
+// 	zoneModel.find({}, function(err, docs) {
+// 		callback(docs);
+// 	});
+// };
