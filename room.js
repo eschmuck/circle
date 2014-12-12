c97c5d66010ce2918c04f0dab28303b329ed7014
eschmuck <e_schmuck@yahoo.com>
2014-12-12 14:54:12 +0000

    modified:   room.js

--- room.js
+++ room.js
@@ -114,6 +114,7 @@ roomSchema.methods.showRoomToCharacter = function(character) {
 	var exits = '';
 
 	console.log(this.northernExit);
+	console.log(typeof(this.northernExit));
 	console.log(this.northernExit == null);
 	console.log(this.northernExit === null);
 	console.log(!this.northernExit);
