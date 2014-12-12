1a4a9f02931f09bc5315ef2e82d65a5387e4405a
eschmuck <e_schmuck@yahoo.com>
2014-12-12 19:02:54 +0000

    new file:   mob.js

---
+++ mob.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+var extend = require('mongoose-schema-extend');
+var character = require("./character");
+var characterSchema = require("./character").schema;
+
+var mobSchema = characterSchema.extend({
+	longDescription: String,
+	detailedDescription: Number,
+	thac0: Number,
+	armorClass: Number,
+	hitpointFormula: String,
+	damRollFormula: String
+});
+
+mobSchema.methods.load = function(id, callback) {
+	mobSchema.find({ id: id }, function(err, docs) {
+		callback(docs);
+	});
+};
+
+var mobModel = mongoose.model('mob', mobSchema);
+
+module.exports = {
+	schema: mobSchema,
+	mob: mobModel
+};
+
