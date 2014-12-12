d17e44c7afde529fe0cc2f8b85652e4766c7bd3c
eschmuck <e_schmuck@yahoo.com>
2014-12-12 19:02:25 +0000

    modified:   zone.js

--- zone.js
+++ zone.js
@@ -1,4 +1,7 @@
 var mongoose = require('mongoose');
+var mob = require('./mob');
+
+
 var schema = mongoose.Schema;
 
 //ar connection = mongoose.connect('mongodb://localhost/circledb');
@@ -15,12 +18,50 @@ var zoneSchema = new schema({
 });
 
 zoneSchema.methods.reset = function(rooms) {
-    for(var i = 0; i < this.resetCommands.length; i++) {
-        //console.log(this.resetCommands[i]);
+    // for(var i = 0; i < this.resetCommands.length; i++) {
+    //     //console.log(this.resetCommands[i]);
+    //     //console.log(this.resetCommands[i].length);
         
-        console.log(this.resetCommands[i].length);
+    //     for(var j = 0; j < this.resetCommands[i].length; j++) {
+    //         var commands = this.resetCommands[i].split(' ');
+            
+    //         switch(commands[0]) {
+    //             case "*":  // Ignore
+    //                 break;
+    //             case "M":  // Mob
+    //                 break;
+    //         }
+    //     }
+    //}
+    
+    for(var i = 0; i < this.resetCommands.length; i++) {
     }
 };
+
+
+function executeZoneResetCommands(commands, instructionNumber, lastNpcLoaded) {
+    if(instructionNumber < commands.length) {
+        var command = commands[instructionNumber].split(" ");
+        
+        // TODO: Determine if load would exceed global quota
+        
+        switch(commands[0]) {
+            case "*":  // ignore
+                break;
+            case "M":  // mobile
+                var thisMob = new mob();
+                //thisMob.id = commands[2];
+                thisMob.load(commands[2], afterMobLoaded);
+                break;
+        }
+    }
+}
+
+function afterMobLoaded(document) {
+    console.log('here');
+    console.log(document);
+}
+
 
 var zoneModel = mongoose.model('zone', zoneSchema);
 
@@ -29,8 +70,3 @@ exports.getZones = function getZones(callback) {
 		callback(docs);
 	});
 };
-
-// module.exports = {
-// 	schema: zoneSchema,
-// 	zone: zoneModel
-// };
