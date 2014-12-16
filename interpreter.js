374b3dce070334da863e47e57ced75d54ed70477
eschmuck <e_schmuck@yahoo.com>
2014-12-16 13:58:03 +0000

    modified:   interpreter.js

--- interpreter.js
+++ interpreter.js
@@ -14,11 +14,9 @@ exports.SCMD_WEST	= 3;
 exports.SCMD_UP		= 4;
 exports.SCMD_DOWN	= 5;
 
-var SCMD_GOSSIP = 2;
-
 module.exports.SCMD_HOLLER  = 0;
 module.exports.SCMD_SHOUT   = 1;
-exports.SCMD_GOSSIP  = SCMD_GOSSIP;
+global.SCMD_GOSSIP  = 2;
 module.exports.SCMD_AUCTION = 3;
 module.exports.SCMD_GRATZ   = 4;
 
