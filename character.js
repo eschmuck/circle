31b261271ed23ef983fabab5aefd892a2e085c60
eschmuck <e_schmuck@yahoo.com>
2014-12-18 15:54:15 +0000

    modified:   character.js

--- character.js
+++ character.js
@@ -558,6 +558,9 @@ characterSchema.methods.findInventoryFromKeywords = function (keyword) {
 			result.token = tokens[1];
 			
 			item = this.findInventoryItem(tokens[0], tokens[1]);
+			
+			console.log('here' + item);
+			
 			result.items.push(item);
 		}
 	}
