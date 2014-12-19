69a297d6bb08b35fa7a40fcd39ac71630057dd3b
eschmuck <e_schmuck@yahoo.com>
2014-12-19 18:09:19 +0000

    modified:   app.js 	modified:   world.js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ room.getRooms(function(roomDocs) {
   });
 });
 
-setInterval(hourElapsed, 10000);
+setInterval(hourElapsed(gameWorld), 10000);
 
 io.sockets.on('connection', function(socket) {
   console.log('A new user connected!');
@@ -224,6 +224,6 @@ function emitMessage(socket, text, color, mask) {
   });
 }
 
-function hourElapsed() {
-  this.gameWorld.hourElapsed();
+function hourElapsed(world) {
+  world.hourElapsed();
 }
