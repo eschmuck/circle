17d264dd5246e25c43ebe3844eb63e30f62603a4
eschmuck <e_schmuck@yahoo.com>
2015-01-05 21:23:26 +0000

    modified:   mudlog.js

--- mudlog.js
+++ mudlog.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 var winstonLogger = new (winston.Logger)({  
     transports: [
         new (winston.transports.Console)({ level: 'debug' }),
-        new (winston.transports.File)({ filename: __dirname + '/../logs/reggie_node.log', level: 'debug' })
+        new (winston.transports.File)({ filename: __dirname + './logs/circle.log', level: 'debug' })
     ]
 });
 
